2a3df30b3ba7c0f1c2d375252a2a14d5ed011d87
高橋　是高 <shcrad.kore@gmail.com>
2019-01-02 08:11:42 +0000

    Userモデルにバリデーションを設置

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_024551) do
+ActiveRecord::Schema.define(version: 2019_01_02_064811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
