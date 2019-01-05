b56c858ca1c0fa1c022f0b0ecb5f6992347bc032
高橋　是高 <shcrad.kore@gmail.com>
2019-01-05 10:07:24 +0000

    eedsにTEXTカラム追加、SessionsHelperをインクルード

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_055536) do
+ActiveRecord::Schema.define(version: 2019_01_05_082224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_01_04_055536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "text"
     t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
